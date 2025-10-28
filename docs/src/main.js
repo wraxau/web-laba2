c0fc160c43c9fe0b41cc122aac2c25b95e0d6032
нина <macbookbro@MacBook-Air-macbookbro.local>
2025-10-28 19:20:42 +0300

    изменила ссылки на файлы

--- docs/src/main.js
+++ docs/src/main.js
@@ -16,5 +16,5 @@ export default defineConfig({
       },
     },
   },
-  base: './', 
+  base: '/web-laba2/', 
 });
--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/web-laba2/',
   build: {
     rollupOptions: {
       input: {
