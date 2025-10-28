a3674b49060781bd0ab809127851e838d096bae6
нина <macbookbro@MacBook-Air-macbookbro.local>
2025-10-28 19:24:08 +0300

    изменила ссылки на файлы

--- docs/src/weather.js
+++ docs/src/weather.js
@@ -81,7 +81,7 @@ async function fetchWeather(city) {
       </details>
       <div class="action-buttons">
         <button id="refreshBtn" class="btn">🔄 Обновить</button>
-        <a href="index.html" class="btn">🏠 На главную</a>
+        <a href="/web-laba2/index.html" class="btn">🏠 На главную</a>
       </div>
     `;
 
